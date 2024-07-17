359911cc97e4e262f2c35fa95dac57ee286fc393
annagracep10 <anna.edwin@people10.com>
2024-07-17 15:20:20 +0530

    All args constructor

--- src/main/java/com/techphantomexample/OrderReciever/dto/LoginDto.java
+++ src/main/java/com/techphantomexample/OrderReciever/dto/LoginDto.java
@@ -1,9 +1,11 @@
 package com.techphantomexample.OrderReciever.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LoginDto {
     private String username;
     private String password;
--- src/main/java/com/techphantomexample/OrderReciever/dto/RegisterDto.java
+++ src/main/java/com/techphantomexample/OrderReciever/dto/RegisterDto.java
@@ -1,8 +1,10 @@
 package com.techphantomexample.OrderReciever.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class RegisterDto {
     private String username;
     private String password;
